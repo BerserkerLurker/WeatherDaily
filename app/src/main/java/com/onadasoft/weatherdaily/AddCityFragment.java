e96bf9fb9d692c106823008a8cca6c24b41b69ee
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2019-03-21 22:33:13 +0100

    feat: add recyclerview, adapter & list of cities to fragment | attach SwipeController//

--- app/src/main/java/com/onadasoft/weatherdaily/AddCityFragment.java
+++ app/src/main/java/com/onadasoft/weatherdaily/AddCityFragment.java
@@ -2,12 +2,26 @@ package com.onadasoft.weatherdaily;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import com.onadasoft.weatherdaily.adapters.AddCityAdapter;
+import com.onadasoft.weatherdaily.models.Coord;
+import com.onadasoft.weatherdaily.models.Current;
+import com.onadasoft.weatherdaily.models.recyclerCities.City;
+import com.onadasoft.weatherdaily.utils.SwipeController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -16,8 +30,14 @@ import android.widget.ImageView;
 public class AddCityFragment extends Fragment implements View.OnClickListener {
 
     ImageView backBtn;
+    RecyclerView rvCities;
+    FloatingActionButton fabAddCity;
+
+    AddCityAdapter addCityAdapter;
 
     public MainActivity mainActivity;
+
+    private List<City> mCities = new ArrayList<>();
 
     public AddCityFragment() {
         // Required empty public constructor
@@ -25,15 +45,44 @@ public class AddCityFragment extends Fragment implements View.OnClickListener {
 
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainActivity = (MainActivity) getActivity();
+        // ToDo -- iterate hashmap and fill cities list
+
+        for(Current current:mainActivity.currentWeatherMap.values()){
+            City city = new City(current.getId(), current.getName(), current.getSys().getCountry(), current.getCoord());
+            mCities.add(city);
+        }
+
+        Log.d("FromAddFrag", "ListCities: "+ mCities.toString());
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mainActivity = (MainActivity) getActivity();
-        Log.d("FromAddFrag: ", mainActivity.currentWeatherMap.toString());
+//        mainActivity = (MainActivity) getActivity();
+//        Log.d("FromAddFrag: ", mainActivity.currentWeatherMap.toString());
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_city, container, false);
         backBtn = view.findViewById(R.id.btn_back_add_city);
         backBtn.setOnClickListener(this);
+
+        fabAddCity = view.findViewById(R.id.fabAddCity);
+        fabAddCity.setOnClickListener(this);
+
+        rvCities = view.findViewById(R.id.rvCities);
+        addCityAdapter = new AddCityAdapter(mCities);
+        rvCities.setLayoutManager(new LinearLayoutManager(mainActivity));
+
+        rvCities.setAdapter(addCityAdapter);
+
+        SwipeController swipeController = new SwipeController();
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(swipeController);
+        itemTouchHelper.attachToRecyclerView(rvCities);
+
         return view;
     }
 
@@ -41,6 +90,12 @@ public class AddCityFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         if(view.getId() == R.id.btn_back_add_city){
 
+        }
+        if(view.getId() == R.id.fabAddCity){
+            // ToDo -- replace with right logic (Dialog box and JSON indexing for available cities)
+            City test = new City(123, "London", "uk", new Coord(10.10, 11.11));
+            mCities.add(test);
+            addCityAdapter.notifyDataSetChanged();
         }
     }
 }
