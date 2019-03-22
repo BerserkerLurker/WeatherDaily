a03e5abb60b7ddf152d7a75c498cc35c09038123
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2019-03-22 17:22:07 +0100

    feat: add SwipeControllerActions

---
+++ app/src/main/java/com/onadasoft/weatherdaily/utils/SwipeControllerActions.java
@@ -0,0 +1,8 @@
+package com.onadasoft.weatherdaily.utils;
+
+public abstract class SwipeControllerActions {
+
+    public void onLeftClicked(int position){}
+
+    public void onRightClicked(int position){}
+}
