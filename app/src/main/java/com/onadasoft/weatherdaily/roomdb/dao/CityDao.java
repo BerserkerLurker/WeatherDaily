f7a87a224df4c8c30ece883cf244d398c6ed156d
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2019-06-29 21:33:39 +0100

    feat: add city DAO

---
+++ app/src/main/java/com/onadasoft/weatherdaily/roomdb/dao/CityDao.java
@@ -0,0 +1,35 @@
+package com.onadasoft.weatherdaily.roomdb.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+
+import com.onadasoft.weatherdaily.models.recyclerCities.City;
+
+import java.util.List;
+
+@Dao
+public interface CityDao {
+    @Query("SELECT * FROM city")
+    List<City> getAll();
+
+    @Query("SELECT * FROM city WHERE name LIKE :name || '%' ORDER BY name ASC LIMIT 10")
+    City findByName(String name);
+
+    @Query("SELECT COUNT(*) FROM city")
+    int getCitySize();
+
+    @Insert
+    void insertAll(City... cities);
+
+    @Insert
+    void insertCity(City city);
+
+    @Delete
+    void deleteCity(City city);
+
+    @Query("DELETE FROM city")
+    public void nukeCities();
+}
