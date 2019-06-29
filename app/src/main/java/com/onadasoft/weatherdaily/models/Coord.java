53faa31c4b077a30ba9984fd2e79a9f505f1c6ef
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2019-06-29 21:32:12 +0100

    feat: add gson fromJson constructor

--- app/src/main/java/com/onadasoft/weatherdaily/models/Coord.java
+++ app/src/main/java/com/onadasoft/weatherdaily/models/Coord.java
@@ -1,8 +1,26 @@
 package com.onadasoft.weatherdaily.models;
 
-public class Coord {
+import com.google.gson.annotations.SerializedName;
+
+import org.json.JSONObject;
+
+import java.io.Serializable;
+
+public class Coord implements Serializable {
+    @SerializedName("lon")
     private double lon;
+
+    @SerializedName("lat")
     private double lat;
+
+    public Coord(){
+
+    }
+
+    public Coord(JSONObject json){
+        this.lon = json.optDouble("lon");
+        this.lat = json.optDouble("lat");
+    }
 
     public Coord(double lon, double lat) {
         this.lon = lon;
