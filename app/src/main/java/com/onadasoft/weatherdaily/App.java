7bb5727d646d8852a819bbab320a8fdf181bcc22
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2019-06-29 21:35:42 +0100

    feat: add populate db from json on start if db empty

--- app/src/main/java/com/onadasoft/weatherdaily/App.java
+++ app/src/main/java/com/onadasoft/weatherdaily/App.java
@@ -1,20 +1,81 @@
 package com.onadasoft.weatherdaily;
 
 import android.app.Application;
+import android.arch.persistence.room.Room;
 import android.content.res.Resources;
+import android.util.Log;
 
-public class App extends Application {
+
+
+import com.onadasoft.weatherdaily.models.recyclerCities.City;
+import com.onadasoft.weatherdaily.roomdb.db.AppDatabase;
+import com.onadasoft.weatherdaily.utils.HelperFunctions;
+import com.onadasoft.weatherdaily.utils.notifRunnable.NotifyingRunnable;
+import com.onadasoft.weatherdaily.utils.notifRunnable.RunnableCompleteListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+public class App extends Application implements RunnableCompleteListener {
+
+    // public int DB_EMPTY = 1;
+
+    private final CountDownLatch startLatch = new CountDownLatch(1);
 
     private static Resources resources;
+
+    private AppDatabase appDB;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         resources = getResources();
+
+        appDB = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "cities-db").build();
+
+        Thread thread = Thread.currentThread();
+        Log.d("threadmain", thread.getName());
+        NotifyingRunnable readCitiesJSON = new NotifyingRunnable() {
+            List<City> citiesList = new ArrayList<>();
+
+            @Override
+            public void doRun() {
+                int dbCitySize = appDB.cityDao().getCitySize();
+                // if db is empty
+                if (dbCitySize == 0){
+                    Thread thread = Thread.currentThread();
+                    Log.d("thread", thread.getName());
+                    citiesList = HelperFunctions.getCitiesFromJSON(getApplicationContext());
+                    Log.d("blob", citiesList.toString());
+                    appDB.cityDao().insertAll(citiesList.toArray(new City[citiesList.size()]));
+                }
+            }
+        };
+        readCitiesJSON.addListener(this);
+        Thread separate = new Thread(readCitiesJSON);
+        separate.start();
+
+
     }
 
     public static Resources getAppResources(){
         return resources;
+    }
+
+    public AppDatabase getDatabase() {
+        return appDB;
+    }
+
+    public CountDownLatch getStartLatch() {
+        return startLatch;
+    }
+
+    @Override
+    public void notifyOfRunnableComplete(Runnable runnable) {
+        // DB_EMPTY = 0;
+        startLatch.countDown();
+
     }
 }
