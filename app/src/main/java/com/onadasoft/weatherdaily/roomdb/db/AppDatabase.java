8cdbfee37ba77f1b5c92d96e2639d51233ddc7af
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2019-07-08 13:55:08 +0100

    feat: change AppDatabase to singleton

--- app/src/main/java/com/onadasoft/weatherdaily/roomdb/db/AppDatabase.java
+++ app/src/main/java/com/onadasoft/weatherdaily/roomdb/db/AppDatabase.java
@@ -3,7 +3,9 @@ package com.onadasoft.weatherdaily.roomdb.db;
 
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 import com.onadasoft.weatherdaily.models.recyclerCities.City;
 import com.onadasoft.weatherdaily.roomdb.dao.CityDao;
@@ -11,4 +13,19 @@ import com.onadasoft.weatherdaily.roomdb.dao.CityDao;
 @Database(entities = {City.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract CityDao cityDao();
+
+    private static volatile AppDatabase INSTANCE;
+
+    public static  AppDatabase getDatabase(final Context context){
+        if (INSTANCE == null){
+            synchronized (AppDatabase.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "cities-db")
+                            .fallbackToDestructiveMigration().build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
