fa281cffb72a22e3e257683cf0be095651b57bf3
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2019-07-11 23:45:55 +0100

    feat: add CitySuggestionAdapter

---
+++ app/src/main/java/com/onadasoft/weatherdaily/adapters/CitySuggestionAdapter.java
@@ -0,0 +1,107 @@
+package com.onadasoft.weatherdaily.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.TextView;
+
+import com.onadasoft.weatherdaily.CityRepository;
+import com.onadasoft.weatherdaily.models.recyclerCities.City;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CitySuggestionAdapter extends ArrayAdapter {
+
+    private List<City> cityList;
+    private Context mContext;
+    private int itemLayout;
+
+    private CityRepository cityRepository = new CityRepository();
+
+    private ListFilter listFilter = new ListFilter();
+
+    public CitySuggestionAdapter(@NonNull Context context, int resource, @NonNull List<City> listOfCities) {
+        super(context, resource, listOfCities);
+        cityList = listOfCities;
+        mContext = context;
+        itemLayout = resource;
+
+    }
+
+    @Override
+    public int getCount() {
+        return cityList.size();
+    }
+
+    @Nullable
+    @Override
+    public City getItem(int position) {
+        return cityList.get(position);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View view, @NonNull ViewGroup parent) {
+        if(view == null){
+            view = LayoutInflater.from(parent.getContext()).inflate(itemLayout, parent, false);
+        }
+
+        TextView cityName = view.findViewById(android.R.id.text1);
+        cityName.setText(getItem(position).getName());
+
+        return view;
+    }
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        return listFilter;
+    }
+
+    public class ListFilter extends Filter{
+
+        private Object lock = new Object();
+
+        @Override
+        protected FilterResults performFiltering(CharSequence prefix) {
+            FilterResults results = new FilterResults();
+            Thread thread = Thread.currentThread();
+            Log.d("Adapter thread1", thread.getName());
+            if(prefix == null || prefix.length() == 0){
+                synchronized (lock){
+                    results.values = new ArrayList<String>();
+                    results.count = 0;
+                }
+            }else {
+                final String searchStrLowerCase = prefix.toString().toLowerCase();
+
+                List<City> matchValues = cityRepository.getCityInfo(mContext, searchStrLowerCase);
+
+                results.values = matchValues;
+                results.count = matchValues.size();
+            }
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            if(results.values != null){
+                cityList = (ArrayList<City>)results.values;
+            }else {
+                cityList = null;
+            }
+            if(results.count > 0){
+                notifyDataSetChanged();
+            }else {
+                notifyDataSetInvalidated();
+            }
+        }
+    }
+}
