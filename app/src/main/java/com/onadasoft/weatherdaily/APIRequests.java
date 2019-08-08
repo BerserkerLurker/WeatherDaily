d8cdc9e47354f9d5a4b88db5dc16394d31392230
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2019-08-08 19:51:14 +0100

    feat: change APIRequests.runGetReq to accept cityId queries and to return Call instead of Response

--- app/src/main/java/com/onadasoft/weatherdaily/APIRequests.java
+++ app/src/main/java/com/onadasoft/weatherdaily/APIRequests.java
@@ -1,13 +1,11 @@
 package com.onadasoft.weatherdaily;
 
-import com.onadasoft.weatherdaily.utils.CallbackFuture;
+import android.util.Log;
 
-import java.util.concurrent.ExecutionException;
-
+import okhttp3.Call;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
 
 public class APIRequests {
 
@@ -15,8 +13,10 @@ public class APIRequests {
     static final String API_URL = App.getAppResources().getString(R.string.base_url);
     static final String APPID = App.getAppResources().getString(R.string.api_key);
 
-    public Response runGetReq(String targetURL, String city, String zip, String country,
-                            String lat, String lon, String cnt, String mode, String units) throws ExecutionException, InterruptedException {
+    //public Response runGetReq(String targetURL, String city, String zip, String country,
+    //                        String lat, String lon, String cnt, String mode, String units) throws ExecutionException, InterruptedException {
+    public Call runGetReq(String targetURL, long id, String city, String zip, String country,
+                          String lat, String lon, String cnt, String mode, String units) {
 
 
         HttpUrl.Builder urlBuilder = HttpUrl.parse(API_URL + targetURL).newBuilder();
@@ -26,29 +26,33 @@ public class APIRequests {
         urlBuilder.addQueryParameter("mode",mode);
 
 
-        String query = "";
+        if (id != 0) {
+            urlBuilder.addQueryParameter("id", Long.toString(id));
+        }else {
 
-        if (validStr(city)) {
-            if (validStr(country)) {
-                query += city + "," + country;
-                urlBuilder.addQueryParameter("q", query);
-            } else {
-                urlBuilder.addQueryParameter("q", city);
+            String query = "";
+
+            if (validStr(city)) {
+                if (validStr(country)) {
+                    query += city + "," + country;
+                    urlBuilder.addQueryParameter("q", query);
+                } else {
+                    urlBuilder.addQueryParameter("q", city);
+                }
+            } else if (validStr(zip)) {
+                if (validStr(country)) {
+                    query += zip + "," + country;
+                    urlBuilder.addQueryParameter("zip", query);
+                } else {
+                    query += zip + ",us";
+                    urlBuilder.addQueryParameter("zip", query);
+                }
+            } else if (validStr(lat) && validStr(lon)) {
+                urlBuilder.addQueryParameter("lat", lat);
+                urlBuilder.addQueryParameter("lon", lon);
             }
-        } else if (validStr(zip)) {
-            if (validStr(country)) {
-                query += zip + "," + country;
-                urlBuilder.addQueryParameter("zip", query);
-            } else {
-                query += zip + ",us";
-                urlBuilder.addQueryParameter("zip", query);
-            }
-        } else if (validStr(lat) && validStr(lon)) {
-            urlBuilder.addQueryParameter("lat", lat);
-            urlBuilder.addQueryParameter("lon", lon);
+
         }
-
-
         if(targetURL.equalsIgnoreCase("/forecast") || targetURL.equalsIgnoreCase("/forecast/daily")) {
             if (validStr(cnt)) {
                 urlBuilder.addQueryParameter("cnt", cnt);
@@ -58,16 +62,18 @@ public class APIRequests {
 
         String url = urlBuilder.build().toString();
 
+        Log.d("request url", url);
+
+
         Request request = new Request.Builder()
                               .url(url)
                               .build();
 
-        CallbackFuture future = new CallbackFuture();
-        client.newCall(request).enqueue(future);
-        Response response = future.get();
-
-
-        return response;
+        // CallbackFuture future = new CallbackFuture();
+        return client.newCall(request);
+        // client.newCall(request).enqueue(future);
+        // Response response = future.get();
+        // return response;
 
 //        client.newCall(request).enqueue(new Callback() {
 //            @Override
