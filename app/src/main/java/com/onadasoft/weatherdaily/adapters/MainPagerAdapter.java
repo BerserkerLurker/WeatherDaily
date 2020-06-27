d2ff9403858106f168e4f0863a6843439eb87764
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2020-06-27 22:05:17 +0100

    use global weathermap and round temperature

--- app/src/main/java/com/onadasoft/weatherdaily/adapters/MainPagerAdapter.java
+++ app/src/main/java/com/onadasoft/weatherdaily/adapters/MainPagerAdapter.java
@@ -6,40 +6,51 @@ import android.support.v4.app.FragmentManager;
 import android.util.Log;
 
 import com.onadasoft.weatherdaily.CityFragment;
+import com.onadasoft.weatherdaily.GlobalData;
 import com.onadasoft.weatherdaily.MainActivity;
 import com.onadasoft.weatherdaily.models.Current;
 import com.onadasoft.weatherdaily.models.CustomCurrent;
 import com.onadasoft.weatherdaily.utils.HelperFunctions;
 
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import static com.onadasoft.weatherdaily.App.getInstance;
+
 public class MainPagerAdapter extends SmartFragmentStatePagerAdapter {
+    private static final String TAG = "MainPagerAdapter";
+    private GlobalData globalData = getInstance().getGlobalData();
 
 
     private int NUM_ITEMS;
-    private LinkedHashMap<String, Current> currentWeatherMap;
+    private LinkedHashMap<String, Current> mCurrentWeatherMap;
+    private Date lastUpdateTimestamp;
 
-    public MainPagerAdapter(FragmentManager fragmentManager, LinkedHashMap<String, Current> currentWeatherMap) {
+    public MainPagerAdapter(FragmentManager fragmentManager, LinkedHashMap<String, Current> currentWeatherMap, Date lastUpdateTimestamp) {
         super(fragmentManager);
-        if(!MainActivity.CWMapEmpty){
-            this.currentWeatherMap = currentWeatherMap;
-            NUM_ITEMS = this.currentWeatherMap.size();
-            Log.d("From Adapter const", getCount()+" "+this.currentWeatherMap.size() );
 
-        }
+        //if(!currentWeatherMap.isEmpty()){
+            mCurrentWeatherMap = currentWeatherMap;
+            this.lastUpdateTimestamp = lastUpdateTimestamp;
+            NUM_ITEMS = mCurrentWeatherMap.size();
+            Log.d("From Adapter const", getCount()+" "+mCurrentWeatherMap.size() );
+
+        //}
     }
 
+    // TODO just notify after changing the global map (not needed???)
     public void setCurrentWeatherMap(LinkedHashMap<String, Current> currentWeatherMap){
-        this.currentWeatherMap = currentWeatherMap;
+        mCurrentWeatherMap = currentWeatherMap;
         notifyDataSetChanged();
-        Log.d("From Adapter setter", getCount()+" "+this.currentWeatherMap.size() );
+        Log.d("From Adapter setter", getCount()+" "+mCurrentWeatherMap.size() );
 
     }
 
 
     @Override
     public Fragment getItem(int position) {
+        /*
 //        switch (position) {
 //            case 0:
 //                return CityFragment.newInstance(0, "Page #1", currentWeatherMap.get("Tunis"));
@@ -50,17 +61,21 @@ public class MainPagerAdapter extends SmartFragmentStatePagerAdapter {
 //            default:
 //                return null;
 //        }
-
-        Current holder = (Current)HelperFunctions.getElementByIndex(currentWeatherMap, position);
+*/
+        Current holder = (Current)HelperFunctions.getElementByIndex(mCurrentWeatherMap, position);
         CustomCurrent customCurrent = new CustomCurrent(holder);
+        customCurrent.setMainTemp(Math.round(customCurrent.getMainTemp()));
+        customCurrent.setMainTempMax(Math.round(customCurrent.getMainTempMax()));
+        customCurrent.setMainTempMin(Math.round(customCurrent.getMainTempMin()));
+        lastUpdateTimestamp = globalData.getLastUpdateTimestamp();
 
-        return CityFragment.newInstance(position, "Page #" + position, customCurrent);
+        return CityFragment.newInstance(position, "Page #" + position, customCurrent, lastUpdateTimestamp);
     }
 
     @Override
     public int getCount() {
-        if (currentWeatherMap!=null) {
-            return currentWeatherMap.size();
+        if (mCurrentWeatherMap!=null) {
+            return mCurrentWeatherMap.size();
         }
         return 0;
 //        return NUM_ITEMS;
